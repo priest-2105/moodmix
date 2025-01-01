2c41cb7523c0b4a7d5eb4b2d9185e193c9e87e58
priest-2105 <priest2105@gmail.com>
2025-01-02 00:13:55 +0100

    style imports

--- server.js
+++ server.js
@@ -3,27 +3,20 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const port = process.env.PORT || 5501;
+const port = process.env.PORT || 5500;
 
 // Serve static files
-app.use(express.static(path.join(__dirname, 'public'), {
-  setHeaders: (res, path, stat) => {
-    if (path.endsWith('.css')) {
-      res.set('Content-Type', 'text/css');
-    }
-  }
-}));
+app.use(express.static(path.join(__dirname, 'public')));
+
 // Endpoint to securely provide the client ID
 app.get('/api/spotify-credentials', (req, res) => {
   res.json({ clientId: process.env.SPOTIFY_APP_CLIENT_ID });
 });
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
-
-
-
-
-
-app.listen(5501, () => {
-  console.log('Server running on http://localhost:5501');
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
 });
