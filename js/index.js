0e90eb2d76d5e48855c4e4c88e1138d8505d90d6
priest-2105 <priest2105@gmail.com>
2025-01-01 23:56:12 +0100

    commit

--- js/index.js
+++ js/index.js
@@ -1,3 +1,13 @@
+import '../css/styles.css';
+import '../css/footer-player.css';
+import '../css/main.css';
+import '../css/navbar-styles.css';
+import '../css/sidebar-styles.css';
+import '../css/section-styles.css';
+import '../css/login.css';
+import '../css/signup.css';
+import '../css/forgotpassword.css';
+
 import auth from './auth.js';
 import playlistLoader from './playlist-loader.js';
 import './login.js';
