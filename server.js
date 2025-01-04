e39cb9248d78a45b9d3cb9d00641ee719e2c29e9
priest-2105 <priest2105@gmail.com>
2025-01-05 00:09:23 +0100

    static

--- server.js
+++ server.js
@@ -1,43 +1,41 @@
-const express = require('express');
-const path = require('path');
-const { clientId, redirectUri, port } = require('./config');
+// const express = require('express');
+// const path = require('path');
+// const { clientId, redirectUri, port } = require('./config');
 
-const app = express();
+// const app = express();
 
-// Log environment variables for debugging
-console.log('SPOTIFY_APP_CLIENT_ID:', clientId);
-console.log('SPOTIFY_APP_REDIRECT_URI:', redirectUri);
+// // Log environment variables for debugging
+// console.log('SPOTIFY_APP_CLIENT_ID:', clientId);
+// console.log('SPOTIFY_APP_REDIRECT_URI:', redirectUri);
 
-// Middleware to add CORS headers
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-  next();
-});
+// // Middleware to add CORS headers
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+//   next();
+// });
 
-// Serve static files
-app.use(express.static(path.join(__dirname, 'public')));
+// // Endpoint to securely provide the client ID and redirect URI
+// app.get('/api/spotify-credentials', (req, res) => {
+//   res.json({ clientId, redirectUri });
+// });
 
-// Endpoint to securely provide the client ID and redirect URI
-app.get('/api/spotify-credentials', (req, res) => {
-  res.json({ clientId, redirectUri });
-});
+// // Serve JavaScript files
+// app.get('/js/:file', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'js', req.params.file));
+// });
 
-// Serve JavaScript files
-app.get('/js/:file', (req, res) => {
-  res.sendFile(path.join(__dirname, 'js', req.params.file));
-});
+// // Serve CSS files
+// app.get('/css/:file', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'css', req.params.file));
+// });
 
-// Serve CSS files
-app.get('/css/:file', (req, res) => {
-  res.sendFile(path.join(__dirname, 'css', req.params.file));
-});
+// // Serve the index.html file from its current location
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'index.html'));
+// });
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
-
-app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
-});
+// app.listen(port, () => {
+//   console.log(`Server running on http://localhost:${port}`);
+// });
